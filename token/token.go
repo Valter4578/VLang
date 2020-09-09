e3fcce7be301cf321a668bfe54d4ef5321651d8e
Valter4578 <mrvalter566@gmail.com>
2020-09-09 23:39:00 +0700

    Add functionallity to read identifiers and integers. Add new tokens and keywords. Write tests for it

--- token/token.go
+++ token/token.go
@@ -10,20 +10,54 @@ type Token struct {
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
+
 	// Identifiers + literals
 	IDENT = "IDENT"
 	INT   = "INT"
+
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
 	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
-	LPAREN    = "("
-	RPAREN    = ")"
-	LBRACE    = "{"
-	RBRACE    = "}"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
+
+var keywoards = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// LookupIdent check keywoards map and determinate identifier is keywoard or not
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywoards[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
