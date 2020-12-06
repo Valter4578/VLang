51f4126897759d983446fc93ef01116a730bde77
Valter4578 <mrvalter566@gmail.com>
2020-12-06 13:08:38 +0700

    Evaluating boolean values

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,10 +5,17 @@ import (
 	"github.com/valter4578/vlang/object"
 )
 
+var (
+	trueRef  = &object.Boolean{Value: true}
+	falseRef = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	case *ast.Program:
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
@@ -23,4 +30,13 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return trueRef
+	} else {
+		return falseRef
+	}
+
 }
