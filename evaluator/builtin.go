e13593920964fc6f8168de8dd68388007fe37b00
Valter4578 <mrvalter566@gmail.com>
2021-02-02 14:26:50 +0700

    Add new builtins functions for arrays: first, last

--- evaluator/builtin.go
+++ evaluator/builtin.go
@@ -2,16 +2,60 @@ package evaluator
 
 import "github.com/valter4578/vlang/object"
 
-var builtins = map[string]*object.BuiltFunc{"len": &object.BuiltFunc{
-	Func: func(args ...object.Object) object.Object {
-		if len(args) != 1 {
-			return newError("wrong number of arguments. got=%d, want=1", len(args))
-		}
-		switch arg := args[0].(type) {
-		case *object.String:
-			return &object.Integer{Value: int64(len(arg.Value))}
-		default:
-			return newError("argument to `len` not supported, got %s", args[0].Type())
-		}
-	}},
+var builtins = map[string]*object.BuiltFunc{
+	"len": &object.BuiltFunc{
+		Func: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.String:
+				return &object.Integer{Value: int64(len(arg.Value))}
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
+			default:
+				return newError("argument to `len` not supported, got %s", args[0].Type())
+			}
+		},
+	},
+	"first": &object.BuiltFunc{
+		Func: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) > 0 {
+				return arr.Elements[0]
+			}
+
+			return NULL
+		},
+	},
+	"last": &object.BuiltFunc{
+		Func: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			lenght := len(arr.Elements)
+			if lenght > 0 {
+				return arr.Elements[lenght-1]
+			}
+
+			return NULL
+		},
+	},
 }
