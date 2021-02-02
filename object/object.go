408d864cad21826e9310a0139f72ebe8d4c0800c
Valter4578 <mrvalter566@gmail.com>
2021-02-02 13:27:52 +0700

    Array evaluation functionality

--- object/object.go
+++ object/object.go
@@ -30,6 +30,8 @@ const (
 	STRING_OBJ = "STRING"
 
 	BUILTFUNC_OBJ = "BUILTINFUNC"
+
+	ARRAY_OBJ = "ARRAY"
 )
 
 // String
@@ -147,4 +149,25 @@ func (b *BuiltFunc) Type() ObjectType {
 
 func (b *BuiltFunc) Inspect() string {
 	return "builtin function"
+}
+
+type Array struct {
+	Elements []Object
+}
+
+func (ao *Array) Type() ObjectType {
+	return ARRAY_OBJ
+}
+func (ao *Array) Inspect() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+	for _, e := range ao.Elements {
+		elements = append(elements, e.Inspect())
+	}
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
 }
