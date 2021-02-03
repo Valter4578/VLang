b70d042d4c987a1247e48657f206275d499c026f
Valter4578 <mrvalter566@gmail.com>
2021-02-03 17:09:34 +0700

    Add "append" builtin function to evaluator module

--- evaluator/builtin.go
+++ evaluator/builtin.go
@@ -58,7 +58,6 @@ var builtins = map[string]*object.BuiltFunc{
 			return NULL
 		},
 	},
-
 	"tail": &object.BuiltFunc{
 		Func: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
@@ -80,6 +79,30 @@ var builtins = map[string]*object.BuiltFunc{
 			}
 
 			return NULL
+		},
+	},
+	"append": &object.BuiltFunc{
+		Func: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2",
+					len(args))
+			}
+
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `tail` must be ARRAY, got %s", args[0].Type())
+			}
+
+			//arr := args[0].(*object.Array)
+			//length := len(arr.Elements)
+			//newElements := make([]object.Object, length+1, length+1)
+			//copy(newElements, arr.Elements)
+			//newElements[length] = args[1]
+
+			arr := args[0].(*object.Array)
+			newElement := args[1]
+			arr.Elements = append(arr.Elements, newElement)
+
+			return arr
 		},
 	},
 }
